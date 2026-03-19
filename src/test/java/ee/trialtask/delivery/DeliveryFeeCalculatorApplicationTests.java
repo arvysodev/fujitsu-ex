dcaee72717c61c2c7d3680a891c17717225b42f7
arvysodev <artjom.vysotski@devtailor.com>
2026-03-19 16:04:46 +0200

    refine project dependencies and test setup

---
+++ src/test/java/ee/trialtask/delivery/DeliveryFeeCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.trialtask.delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryFeeCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
